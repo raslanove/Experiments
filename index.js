953344c8fa998c8bcc2c2e2fe1a2b9ae2978435f
Omar El Sayyed <omar@nomone.com>
2018-12-01 21:49:31 +0200

    - An attempt to force a border around ANY page without affecting it's style.

--- index.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('load', function() { 
-
-    var div = document.getElementById("myDiv");
-
-    setInterval(function() {
-        console.log(window.getComputedStyle(div).getPropertyValue("background-color") );
-    }, 50);
-});
